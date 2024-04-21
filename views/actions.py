eab4f7265c2a770751c00bd6bb5038f303fca2df
hristochavez <hristoandychavezhuertas@gmail.com>
2024-04-21 17:19:16 -0500

    Vista de registro de empleado y marcación de hora

--- views/actions.py
+++ views/actions.py
@@ -1,4 +1,5 @@
 import time
+from colorama import Fore
 import views.login as vl
 import templates.employee_menu as te
 import models.employees as me
@@ -7,10 +8,12 @@ import models.employees as me
 # Envia los datos para realizar la marcación de un empleado.
 def create_marktime(dni):
     if me.create_marktime(dni):
-        print('Marcación realizada correctamente.')
+        print(Fore.GREEN + 'Marcación realizada correctamente.' + Fore.WHITE)
+        time.sleep(1.5)
     else:
-        print('Ocurrio un error al realizar la marcación.')
-    time.sleep(1)
+        print(Fore.RED + 'Ocurrio un error al realizar la marcación. Vuelva '
+                         'a intentarlo' + Fore.WHITE)
+        time.sleep(2.5)
 
     # Redirección al menú de empleados.
     te.employee_menu(vl.logged_employee)
@@ -19,11 +22,12 @@ def create_marktime(dni):
 # Envia los datos necesarios para registrar a un empleado.
 def create_employee(employee_info):
     if me.create_employee(employee_info):
-        print('Empleado creado correctamente.')
-        time.sleep(1)
+        print(Fore.GREEN + 'Empleado registrado correctamente.' + Fore.WHITE)
+        time.sleep(1.5)
     else:
-        print('Ocurrio un error al realizar la creación. Vuelva a intentarlo.')
-        time.sleep(2)
+        print(Fore.RED + 'Ocurrio un error al registrar al empleado. Vuelva a '
+                         'intentarlo' + Fore.WHITE)
+        time.sleep(2.5)
 
     # Redirección al menú de empleados.
     te.employee_menu(vl.logged_employee)
