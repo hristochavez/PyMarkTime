1956e8f71f24215e465a2cab191cef4a594b3817
hristochavez <hristoandychavezhuertas@gmail.com>
2024-04-25 20:54:58 -0500

    Refactorización de código

--- templates/employee_menu.py
+++ templates/employee_menu.py
@@ -2,6 +2,7 @@ import re
 import time
 import views.actions as va
 import views.login as vl
+import views.validations as vv
 from colorama import Fore
 from templates.banner import banner
 from templates.commons import clean_screen
@@ -18,39 +19,14 @@ actions = {
 }
 
 
-# Validación de DNI.
-# DNI: Compuesta de 8 dígitos.
-def validate_dni(dni):
-    pattern = r'^\d{8}$'
-    while not re.match(pattern, dni):
-        print(Fore.RED + 'Ingrese un DNI valido. Solo debe de tener 8 '
-                         'dígitos.' + Fore.WHITE)
-        dni = input('Ingrese su DNI:')
-
-    return dni
-
-
-# Validación del nombre.
-# Nombre: Solo debe de tener letras.
-def validate_first_name(first_name):
-    pattern = r'^[a-zA-Z]+$'
-    while not re.match(pattern, first_name):
-        print(Fore.RED + 'Ingrese un nombre válido. Solo debe de tener '
-                         'letras.' + Fore.WHITE)
-        first_name = input('Ingrese su nombre:')
-
-    return first_name
-
-
 # Recoge la información necesaria para crear a un empleado.
-def new_employee():
+def format_new_employee():
     dni = input('Ingrese el DNI del empleado (8 caracteres): ')
-    dni = validate_dni(dni)
+    dni = vv.dni(dni)
 
     first_name = input('Ingrese el nombre del empleado (solo debe contener '
                        'letras): ')
-
-    first_name = validate_first_name(first_name)
+    first_name = vv.first_name(first_name)
 
     # Formatea la información del nuevo empleado.
     employee = {
@@ -62,37 +38,54 @@ def new_employee():
 
 
 # Procesa la inhabilitación de un empleado.
-def disable_employee(result, dni_employee_to_disable, dni):
+def disable_employee(result, employee_to_disable, dni):
     if result is None:
         print(Fore.RED, end='')
-        print(f'El empleado con DNI {dni_employee_to_disable} no existe.')
+        print(f'El empleado con DNI {employee_to_disable} no existe.')
         print(Fore.WHITE, end='')
         time.sleep(2.5)
     elif result[1] == 0:
         print(Fore.RED, end='')
-        print(f'El empleado {result[0]} con DNI {dni_employee_to_disable}'
+        print(f'El empleado {result[0]} con DNI {employee_to_disable}'
               ' ya se encuentra inhabilitado.')
         print(Fore.WHITE, end='')
         time.sleep(2.5)
     elif result[1] == 1:
         print(Fore.YELLOW, end='')
-        disable_emp = input(f'¿Desea inhabilitar al empleado {result[0]} con '
+        response = input(f'¿Desea inhabilitar al empleado {result[0]} con '
                             'DNI {dni_employee_to_disable}? Escriba S o N: ')
         print(Fore.WHITE, end='')
 
-        while disable_emp not in ['S', 's', 'n', 'N']:
-            disable_emp = input('Escriba S o N para inhabilitar al empleado.')
+        response = vv.response_disable(response)
 
-        if disable_emp == 's' or disable_emp == 'S':
-            va.disable_employee(dni_employee_to_disable, dni)
+        if response == 's' or response == 'S':
+            va.disable_employee(employee_to_disable, dni)
 
-    employee_menu(vl.logged_employee)
+    menu(vl.logged_employee)
+
+
+# Retorna una lista de acciones que puede realizar un usuario donde cada
+# acciónn es una tupla conformada por:
+# (número de opción a mostrar, llave de la acción, descripción de la acción).
+def get_employee_actions(employee_permissions):
+    # Lista de acciones permitidas.
+    employee_actions = []
+
+    # Llena la lista de acciones permitidas de acuerdo a los permisos que
+    # tiene el usuario.
+    order = 1
+    for id_action, description in actions.items():
+        if id_action in employee_permissions:
+            employee_actions.append((order, id_action, description))
+            order += 1
+
+    return employee_actions
 
 
 # Ejecuta la acción elegida por el usuario.
 # Obtiene la acción a realizar de la lista de acciones que puede realizar
 # el usuario.
-def action_to_do(employee_actions, selected_action, dni=None):
+def execute_action(employee_actions, selected_action, dni=None):
     do = 0
 
     for action in employee_actions:
@@ -103,40 +96,22 @@ def action_to_do(employee_actions, selected_action, dni=None):
     if do == 1:
         va.create_marktime(dni)
     elif do == 2:
-        va.create_employee(new_employee())
+        va.create_employee(format_new_employee())
     elif do == 3:
         # Solicita el dni del empleado a inhabilitar.
-        dni_employee_to_disable = input('Ingrese el DNI del empleado'
+        employee_to_disable = input('Ingrese el DNI del empleado'
                                         ' (8 caracteres): ')
-        dni_employee_to_disable = validate_dni(dni_employee_to_disable)
+        employee_to_disable = vv.dni(employee_to_disable)
 
         # Resultado de la solicitud de la información del empleado.
-        result = va.employee_information(dni_employee_to_disable)
+        result = va.employee_information(employee_to_disable)
 
         # Inhabilitación del empleado.
-        disable_employee(result, dni_employee_to_disable, dni)
-
-
-# Retorna una lista de acciones que puede realizar un usuario donde cada
-# acciónn es una tupla conformada por:
-# (número de opción a mostrar, llave de la acción, descripción de la acción).
-def get_actions(permissions):
-    # Lista de acciones permitidas.
-    permitted_actions = []
-
-    # Llena la lista de acciones permitidas de acuerdo a los permisos que
-    # tiene el usuario.
-    num_opt = 1
-    for key, value in actions.items():
-        if key in permissions:
-            permitted_actions.append((num_opt, key, value))
-            num_opt += 1
-
-    return permitted_actions
+        disable_employee(result, employee_to_disable, dni)
 
 
 # Muestra el meńu principal del empleado.
-def employee_menu(employee):
+def menu(employee):
     clean_screen()
     banner()
 
@@ -148,18 +123,14 @@ def employee_menu(employee):
     print('Usted tiene los siguientes permisos:')
     # employee['permissions'] contiene una tupla con los id de los permisos
     # que tiene el usuario.
-    employee_actions = get_actions(employee['permissions'])
+    employee_actions = get_employee_actions(employee['permissions'])
     for action in employee_actions:
         print(f'{action[0]}.- {action[2]}')
 
     # Pregunta al usuario por la acción a realizar.
-    # Cantidad de acciones permitidas.
-    qty_actions = [str(x) for x in range(1, len(employee_actions) + 1)]
-
     print('')
     selected_action = input('Elija el número de acción a realizar: ')
-    while selected_action not in qty_actions:
-        selected_action = input('Elija el número de acción a realizar: ')
+    selected_action = vv.selected_action(len(employee_actions), selected_action)
 
     # Realiza la acción seleccionada por el usuario.
-    action_to_do(employee_actions, selected_action, employee['dni'])
+    execute_action(employee_actions, selected_action, employee['dni'])
--- views/login.py
+++ views/login.py
@@ -1,7 +1,7 @@
-from models.employees import get_employee
-from templates.employee_menu import employee_menu
-import templates.login
 import hashlib
+import templates.login as tl
+import templates.employee_menu as tem
+from models.employees import get_employee
 
 # Almacena la información del empleado que inició sesión.
 logged_employee = {}
@@ -13,7 +13,7 @@ def hash_password(employee_pass):
 
 
 # Formatea la información retornada de la consulta para crear a un empleado.
-def format_data_employee(data):
+def format_employee(data):
     dni = data[0][0]
     first_name = data[0][1]
     permissions = []
@@ -35,12 +35,12 @@ def format_data_employee(data):
 # Envia los datos para un inicio de sesión de un empleado.
 def login_employee(dni, password):
     # Comunicación con el modelo.
-    result = get_employee(dni, hash_password(password))
+    query_result = get_employee(dni, hash_password(password))
 
-    # Si retorna más de un empleado llama al menú de empleados.
-    if len(result):
-        employee_menu(format_data_employee(result))
+    # Si retorna a un empleado, invoca al menú de empleados.
+    if len(query_result):
+        tem.menu(format_employee(query_result))
     else:
         # Si no retorna ningun resultado vuelve a mostrar el menú de inicio de
         # sesión.
-        templates.login.login_screen(fail=True)
+        tl.login_screen(fail=True)
