552318c4c8bff24ab514a8dd6f418389d67f8195
hristochavez <142743253+hristochavez@users.noreply.github.com>
2024-04-27 16:24:44 -0500

    Redefinir la tabla de marcaciones (markings) (#12)

--- pymarktime_sql_scripts/procedures.sql
+++ pymarktime_sql_scripts/procedures.sql
@@ -1,52 +1,4 @@
--- Usar la BBDD pymarktime.
 USE pymarktime;
-
-------------
--- TABLAS --
-------------
-
--- Creación de tabla de empleados.
-CREATE TABLE employees(
-	dni CHAR(8) NOT NULL,
-	first_name VARCHAR(30) NOT NULL,
-    second_name VARCHAR(30),
-    last_name VARCHAR(30) NOT NULL,
-    second_last_name VARCHAR(30) NOT NULL,
-	pass CHAR(64) NOT NULL,
-	is_enabled TINYINT(1) DEFAULT 1 NOT NULL,
-	updated_by CHAR(8),
-	CONSTRAINT pk_dni PRIMARY KEY (dni),
-	CONSTRAINT fk_dni_updated_by FOREIGN KEY (updated_by) REFERENCES employees(dni)
-);
-
--- Creación de tabla de marcaciones.
-CREATE TABLE markings(
-	id INT NOT NULL AUTO_INCREMENT,
-	dni CHAR(8) NOT NULL,
-	mark_time DATETIME NOT NULL,
-	CONSTRAINT pk_id PRIMARY KEY (id),
-	CONSTRAINT fk_dni_marking FOREIGN KEY (dni) REFERENCES employees(dni)
-);
-
--- Creación de tabla permisos.
-CREATE TABLE permissions(
-	id INT NOT NULL AUTO_INCREMENT,
-	description VARCHAR(255),
-	CONSTRAINT pk_id PRIMARY KEY (id)
-);
-
--- Creación de tabla que 	relaciona empleados y sus permisos.
-CREATE TABLE employee_permission(
-	dni CHAR(8) NOT NULL,
-	permission INT NOT NULL,
-	CONSTRAINT pk_dni_permission PRIMARY KEY (dni, permission),
-	CONSTRAINT fk_dni FOREIGN KEY (dni) REFERENCES employees(dni) ON UPDATE CASCADE,
-	CONSTRAINT fk_permissions FOREIGN KEY (permission) REFERENCES permissions(id)
-);
-
---------------------------------
--- PROCEDIMIENTOS ALMACENADOS --
---------------------------------
 
 -- Retorna al empleado para un inicio de sesíon.
 DROP PROCEDURE IF_EXISTS get_employee;
@@ -65,8 +17,8 @@ BEGIN
 		employees AS t1
 	INNER JOIN
 		employee_permission AS t2
-	ON
-		t1.dni = t2.dni
+	USING
+		(dni)
     WHERE
     	t1.dni = dni AND 
         t1.pass = pass AND
@@ -78,11 +30,12 @@ DELIMITER ;
 DROP PROCEDURE IF_EXISTS create_marktime;
 DELIMITER $
 CREATE PROCEDURE create_marktime(
-    IN dni CHAR(8)
+    IN dni CHAR(8),
+    IN marked_by CHAR(8)
 )
 BEGIN
-	INSERT INTO markings(dni, mark_time)
-	VALUES (dni, NOW());
+	INSERT INTO markings(dni, marked_by, mark_time)
+	VALUES (dni, marked_by, NOW());
 END $
 DELIMITER ;
 
@@ -128,7 +81,7 @@ BEGIN
 END $
 DELIMITER ;
 
--- Retorna la información de un empleado.
+-- Retorna la información de un empleado`
 DROP PROCEDURE IF_EXISTS employee_information;
 DELIMITER $
 CREATE PROCEDURE employee_information(
